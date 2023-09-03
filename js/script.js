8f4fbc03e26769582ded9a5adb91a5dd0356dc0d
Nathan Carqueijeiro <nathancarqueijeiroetd@gmail.com>
2023-09-03 22:53:53 +0200

    ajout Nathan de pages

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 // creer une alerte ppour testez que le javascript est bien relié a la page html
 console.log("ca marche")
 
-// Ajoutez un écouteur d'événements à chaque élément d'accordéon
-const accordeonItems = document.querySelectorAll('.accordeon-item');
+function initAccordeon() {
+  const accordeonItems = document.querySelectorAll('.accordeon-item');
 
   accordeonItems.forEach(item => {
     const header = item.querySelector('.accordeon-header');
@@ -25,3 +25,64 @@ const accordeonItems = document.querySelectorAll('.accordeon-item');
       }
     });
   });
+}
+
+// fonction pour chargez le script de scroll
+
+function loadScript(url, callback) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  script.async = true;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+// Fonction pour charger les scripts lorsque les éléments deviennent visibles
+function loadScriptsOnScroll() {
+  var elements = document.querySelectorAll('[data-js-src]');
+
+  elements.forEach(function(element) {
+      var rect = element.getBoundingClientRect();
+
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          var scriptSrc = element.getAttribute('data-js-src');
+          
+          // Charger le script JavaScript associé à l'élément
+          loadScript(scriptSrc, function() {
+              // Supprimer l'attribut data-js-src pour éviter de charger le script à nouveau
+              element.removeAttribute('data-js-src');
+          });
+      }
+  });
+}
+
+
+// Écouter l'événement de défilement de la page
+window.addEventListener('scroll', loadScriptsOnScroll);
+
+// Appeler la fonction pour charger les scripts initiaux lorsque la page se charge
+window.addEventListener('load', function() {
+  loadScriptsOnScroll();
+});
+
+
+// Appeler la fonction pour initialiser l'accordéon
+
+function highlightCurrentPage() {
+  const navLinks = document.querySelectorAll('.nav-links');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(otherLink => {
+            otherLink.classList.remove('active');
+        });
+        link.classList.add('active');
+    });
+});
+}
+
+// Appelle la fonction lors du chargement de la pag
+
+initAccordeon();
+highlightCurrentPage();
