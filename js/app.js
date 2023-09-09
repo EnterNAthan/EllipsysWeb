64a0447bf59c0afa0850f6c15a5a6aeb084dacc6
Nathan Carqueijeiro <nathancarqueijeiroetd@gmail.com>
2023-09-09 15:30:43 +0200

    ajout d'animation 2.0

--- js/app.js
+++ js/app.js
@@ -25,6 +25,15 @@
 
 const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 
+    // Animation du titre
+    tl
+    .from(".l1", { width: 0, duration: 1 })
+    .from(".l2", { width: 0, duration: 1 }, "-=0.5")
+    .from("#logo_elypsis", { scale: 0, duration: 0.5 }, "-=0.5")
+    .from(".nav-links", { opacity: 0, stagger: 0.2 }, "-=0.5")
+    
+
+    tl.from("h1",{opacity: 0, x: -100, stagger: 1});
     // Animation du texte
     tl.from("h1 span", { opacity: 0, y: -50, stagger: 1 });
 
