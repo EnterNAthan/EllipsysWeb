7fdf31ce9fbdea21eb7b8304d2c2589b32976a7f
Nathan Carqueijeiro <nathancarqueijeiroetd@gmail.com>
2023-10-22 22:35:20 +0200

    update seo et correctif xml sitemap html et shemas.org + autres choses

---
+++ js/shemas.js
@@ -0,0 +1,23 @@
+<script type="application/ld+json">
+{
+   "@context": "http://schema.org",
+   "@type": "Organization",
+   "name": "Ellipsys",
+   "url": "https://ellipsys-bi.com/index.html",
+   "logo": "https://ellipsys-bi.com/images/logotr.png",
+   "description": "Ellipsys est l'éditeur d'openAudit, un logiciel de gouvernance des données qui s'appuie sur le data lineage technique. ",
+   "address": {
+     "@type": "PostalAddress",
+     "streetAddress": "29 Route d'Esch",
+     "addressLocality": "Ville",
+     "addressRegion": "Bettembourg",
+     "postalCode": "L-3230",
+     "addressCountry": "Luxembourg"
+   },
+   "contactPoint": {
+     "@type": "ContactPoint",
+     "telephone": "+XX-XXXXXXX",
+     "contactType": "Support client"
+   }
+}
+</script>
